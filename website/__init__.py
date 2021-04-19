da017dadb0d389f1cda75494d5e0fa4709275fe4
Francesco Piccoli <70111810+francescoopiccoli@users.noreply.github.com>
2021-04-19 15:16:04 +0200

    database initial configuration

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,17 @@
 #this file makes the website folder a python package, so that when we import the websit folder, and wheether is in the init file runs automatically when importing the folder
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+#DB_NAME = "database.db"
 
 def create_app():
 
     app = Flask(__name__)
     #app.config['SECRET_KEY'] = ''  #for securing session data 
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'{DB_NAME}' #here goes the URI of the database
+    db.init_app(app)
+
     from .views import views 
     from .auth import auth 
 
