b8b7f0dec4c869ab1dae2d1230f7398eab230734
Francesco Piccoli <70111810+francescoopiccoli@users.noreply.github.com>
2021-04-20 10:47:09 +0200

    database creation

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,20 @@
 #this file makes the website folder a python package, so that when we import the websit folder, and wheether is in the init file runs automatically when importing the folder
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
-#DB_NAME = "database.db"
+DB_NAME = "database.db"
 
-def create_app():
-
+def create_database(app):
+        if not path.exists('website/' + DB_NAME):
+            db.create_all(app=app)
+            print('Created Database!')
+            
+def create_app(): 
     app = Flask(__name__)
     #app.config['SECRET_KEY'] = ''  #for securing session data 
-    #app.config['SQLALCHEMY_DATABASE_URI'] = f'{DB_NAME}' #here goes the URI of the database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
     from .views import views 
@@ -18,5 +23,14 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import User, Post, Comment   
+    
+    create_database(app)
+
     return app
+
+    
+
+
+ 
     
