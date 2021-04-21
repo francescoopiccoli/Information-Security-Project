f82bd61a98d504dd08f3b27808a104d0798aaa59
mattf13 <63061267+mattf13@users.noreply.github.com>
2021-04-21 15:08:11 +0200

    modified login layout (same as sign-up)

--- website/views.py
+++ website/views.py
@@ -6,11 +6,3 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
-
-@views.route('/sign-up')
-def sign_up():
-    return render_template("sign_up.html")
-
-@views.route('/login')
-def login():
-    return render_template("login.html")
