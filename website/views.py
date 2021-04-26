6e470614cf6cec3937505531d54cc3c2c43d51d0
Francesco Piccoli <70111810+francescoopiccoli@users.noreply.github.com>
2021-04-26 09:57:26 +0200

    added logic for home page

--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template
 from flask import url_for
 from flask_login import login_required, current_user
-
+from .models import Post
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("home.html", user=current_user)
+    posts = Post.query.all()
+    return render_template("home.html", user=current_user, posts=posts)
