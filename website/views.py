44eaee3d0fd43d4df1e6c84c9cc6e0720d65c2d9
mattf13 <63061267+mattf13@users.noreply.github.com>
2021-04-30 15:12:32 +0200

    updated post creator in home

--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template
 from flask import url_for
 from flask_login import login_required, current_user
-from .models import Post
+from .models import Post, User
 views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def home():
     posts = Post.query.all()
-    return render_template("home.html", user=current_user, posts=posts)
+    userID_username_dict = dict()
+    for post in posts:
+        userID_username_dict.update({post.user_id: User.query.get(post.user_id).username})
+    return render_template("home.html", user=current_user, posts=posts, usernames=userID_username_dict)
