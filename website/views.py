4df2d937ee5191d0c8ee6929bc9bea576cba85c6
Francesco Piccoli <70111810+francescoopiccoli@users.noreply.github.com>
2021-05-18 10:58:07 +0200

    search backend + hom.html major refactoring

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask import url_for
+from flask import request
 from flask_login import login_required, current_user
 from .models import Post, User
 views = Blueprint('views', __name__)
@@ -8,7 +9,22 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     posts = Post.query.all()
+    #if request.method == 'GET':
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(Post.post_title.contains(q) | Post.post_text.contains(q)).all()
+        #in case no resuslts are found, the list of all the posts is shown
+        if len(posts) == 0:
+            posts = Post.query.all()
+
+    else:
+        posts = Post.query.all()
+        print(posts)
+
     userID_username_dict = dict()
     for post in posts:
         userID_username_dict.update({post.user_id: User.query.get(post.user_id).username})
     return render_template("home.html", user=current_user, posts=posts, usernames=userID_username_dict)
+
+    return render_template("home.html", user=current_user, posts=posts, usernames=userID_username_dict, search_content=q)
